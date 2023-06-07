927ae7a57efe01ae17023eaa6ccec9adf72bb288
Wesley Ramos Tavares <wesley.tavares@meta.com.br>
2023-06-07 13:18:02 -0300

    feat: criada a autenticação de usuário, permitindo o login livre ao login e os demais métodos apenas com token de autenticação.

--- src/main/java/br/com/med/voll/api/service/TokenService.java
+++ src/main/java/br/com/med/voll/api/service/TokenService.java
@@ -4,6 +4,7 @@ import br.com.med.voll.api.model.Usuario;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -27,6 +28,20 @@ public class TokenService {
                     .sign(algoritmo);
         } catch (JWTCreationException exception){
             throw new RuntimeException("erro ao gerar token jwt", exception);
+        }
+    }
+
+
+    public String getSubject(String tokenJWT){
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("vollmed_api")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token JWT inválido ou expirado");
         }
     }
 
